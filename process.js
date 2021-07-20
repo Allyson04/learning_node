b0dbec481b168b9b4e922e0ba7443a7b2d25ac2b
Allyson-mkw <allysoneduardo04@gmail.com>
2021-07-19 21:00:11 -0300

    feat: learning how to acess processing list

---
+++ process.js
@@ -0,0 +1,9 @@
+// // processing list
+// console.log(process)
+
+// //processing list args
+// console.log(process.argv)
+
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+console.log(`Seu nome é ${firstName} ${lastName}`)
